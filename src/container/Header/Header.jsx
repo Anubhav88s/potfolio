a693b75bd335fe5e536eb6af41b2ad1a31347d69
Anubhav <anubhavrajsinghu@gmail.com>
2025-06-28 02:32:01 +0530

    nextjs symbol

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
       whileInView={scaleVarients.whileInView}
       className='app__header-circles'
       >
-        {[images.flutter , images.redux , images.sass].map((circle, index)=>(
+        {[images.flutter , images.Nextjs , images.sass].map((circle, index)=>(
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt='circle'/>
           </div>
